429d9a25987f77470496ffdaf7546ffc5db40095
Matt <98845506+mutthias@users.noreply.github.com>
2023-09-03 14:22:39 -0700

    Creating accounts and logging in
    title says all

--- client/src/App.js
+++ client/src/App.js
@@ -1,98 +1,27 @@
 import {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from './Components/Home';
+import Signup from './Components/Signup';
 
-const API_BASE = 'http://localhost:3001';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [popup, Setpopup] = useState(false);
-  const [newTodo, setNewTodo] = useState("");
-
   
-
-  useEffect(() => {
-    GetTodos();
-    console.log(todos)
-  }, [])
-
-  const GetTodos = () => {
-		fetch(API_BASE + '/todos')
-			.then(res => res.json())
-			.then(data => setTodos(data))
-			.catch((err) => console.error("Error: ", err));
-	}
-  
-  const completeTodo = async id => {
-    const data = await fetch(API_BASE + "/todo/complete/" + id)
-      .then(res => res.json());
-    setTodos(todos => todos.map(todo => {
-      if (todo._id === data._id) {
-        todo.complete = data.complete;
-      }
-      return todo;
-    }));
-  }
-
-  const deleteTodo = async id => {
-    const data = await fetch(API_BASE + "/todo/delete/" + id, { method: "DELETE"})
-      .then(res => res.json());
-
-      setTodos(todos => todos.filter(todo => todo._id !== data._id));
-  }
-
-  const addTodo = async () => {
-    const data = await fetch(API_BASE + "/todo/new", {
-      method: "POST",
-      headers: {
-        "Content-Type" : "application/json"
-      },
-      body : JSON.stringify({
-        text: newTodo
-      })
-    }).then(res => res.json());
-    
-    setTodos([...todos, data]);
-    Setpopup(false);
-    setNewTodo("");
-  }
 
   return (
+
     <div className="App">
-      <h1>Hello MattyB</h1>
-      <h4>Your Tasks</h4>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Login/>}/> */}
+          <Route path="/" element={<Navigate to="/Home" />} />
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/Signup" element={<Signup/>}/>
 
-      <div className='todos'>
-        {
-          todos.map(todo => (
-            <div className={"todo " + (todo.complete ? "is-complete" : "")
-            } key={todo._id} onClick={() => completeTodo(todo._id)}>
-              <div className='checkbox'></div>
-              <div className='text'>{todo.text}</div>
-              <div className='delete-todo' onClick={() => deleteTodo(todo._id)}>x</div>
-            </div>
-          ))
-        }
-        
-      </div>
-      <div className='addPopup' onClick={() => Setpopup(true)}>+</div>
-
-      {popup ? (
-        <div className='popup'>
-          <div className='closePopup' onClick={() => 
-            Setpopup(false)}>x</div>
-            <div className='content'>
-              <h3>Add Task</h3>
-              
-              <input 
-                type='text' 
-                className='add-todo-input' 
-                onChange={e => setNewTodo(e.target.value)}
-                value={newTodo} />
-              <div className='button' onClick={addTodo}>Create Task</div> 
-            </div>
-        </div>
-      ) : ''}
-
+        </Routes>
+      </Router>
     </div>
+
+    
   );
 }
 
